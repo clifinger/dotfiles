fef6fb9ed715e253454738489992534d49683ef7
Julien Lenne <contact.lenne@gmail.com>
2025-06-30 12:24:15 +0800

    feat: add zsh plugin installation for autosuggestions and syntax-highlighting

--- install.sh
+++ install.sh
@@ -62,6 +62,24 @@ else
     warn "Oh My Zsh is already installed."
 fi
 
+# --- Oh My Zsh Plugin Installation ---
+
+log "Installing Oh My Zsh plugins..."
+
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+else
+    warn "zsh-autosuggestions already installed."
+fi
+
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+else
+    warn "zsh-syntax-highlighting already installed."
+fi
+
 # --- Clone Personal Repositories ---
 
 log "Cloning personal configuration repositories..."
